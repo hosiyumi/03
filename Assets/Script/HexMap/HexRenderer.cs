760d72b7b64107f9c0cef6bf5a8b5453d8838903
hosiyumi <1027534745@qq.com>
2026-03-28 18:01:38 +0800

    规范化重构与管理
    重写了管理规范与代码风格，emmm，直接看例子不就更好理解了吗

--- Assets/Script/HexMap/HexRenderer.cs
+++ Assets/Script/HexMap/HexRenderer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//V1采用了从正方形切割出正六边形的实现，这一版采用了组合的思想，更多变；
+//单格渲染
 
 public struct Face//单格绘制
 {
@@ -22,16 +22,16 @@ public struct Face//单格绘制
 [RequireComponent(typeof(MeshRenderer))]
 //请求组件
 
-public class HexRendererV2 : MonoBehaviour
+public class HexRenderer : MonoBehaviour
 {
     private Mesh m_mesh;
     private MeshFilter m_meshFilter;
     private MeshRenderer m_meshRenderer;
 
-    public List<Face> m_faces; 
+    public List<Face> m_faces;
 
     public Material material;
-    public float innersize,outersize,height;
+    public float innersize, outersize, height;
     public bool isFlatTopped;
 
     private void Awake()
@@ -64,19 +64,19 @@ public class HexRendererV2 : MonoBehaviour
         DrawFaces();
         CombineFaces();
     }
- 
+
     public void DrawFaces()
     {
         m_faces = new List<Face>();
         //顶面
-        for (int point = 0;point < 6 ; point++)
+        for (int point = 0; point < 6; point++)
         {
-            m_faces.Add(CrateFace(innersize, outersize, height / 2f,height / 2f,point));
+            m_faces.Add(CrateFace(innersize, outersize, height / 2f, height / 2f, point));
         }
         //底面
         for (int point = 0; point < 6; point++)
         {
-            m_faces.Add(CrateFace(innersize, outersize, -height / 2f, -height / 2f,point,true));
+            m_faces.Add(CrateFace(innersize, outersize, -height / 2f, -height / 2f, point, true));
         }
         //外面
         for (int point = 0; point < 6; point++)
@@ -91,30 +91,30 @@ public class HexRendererV2 : MonoBehaviour
 
     }
 
-    private Face CrateFace(float innerRad, float outerRead,float heightA,float heightB,int point,bool revese = false)
+    private Face CrateFace(float innerRad, float outerRead, float heightA, float heightB, int point, bool revese = false)
     {
-        Vector3 pointA = GetPoint(innerRad,heightB,point);
-        Vector3 pointB = GetPoint(innerRad,heightB, (point < 5) ? point + 1 : 0);
-        Vector3 pointC = GetPoint(outerRead,heightA,(point < 5) ? point + 1 : 0);
-        Vector3 pointD = GetPoint(outerRead,heightA,point);
+        Vector3 pointA = GetPoint(innerRad, heightB, point);
+        Vector3 pointB = GetPoint(innerRad, heightB, (point < 5) ? point + 1 : 0);
+        Vector3 pointC = GetPoint(outerRead, heightA, (point < 5) ? point + 1 : 0);
+        Vector3 pointD = GetPoint(outerRead, heightA, point);
 
-        List<Vector3> vertices = new List<Vector3>(){pointA,pointB,pointC,pointD};
-        List<int> triangles = new List<int>(){0,1,2,2,3,0};
-        List<Vector2> uvs =new List<Vector2>(){new Vector2(0,0), new Vector2(1,0), new Vector2(1, 1), new Vector2(0, 1) };
+        List<Vector3> vertices = new List<Vector3>() { pointA, pointB, pointC, pointD };
+        List<int> triangles = new List<int>() { 0, 1, 2, 2, 3, 0 };
+        List<Vector2> uvs = new List<Vector2>() { new Vector2(0, 0), new Vector2(1, 0), new Vector2(1, 1), new Vector2(0, 1) };
 
         if (revese)
         {
             vertices.Reverse();
         }
 
-        return new Face(vertices,triangles,uvs);
+        return new Face(vertices, triangles, uvs);
     }
 
-    protected Vector3 GetPoint(float size,float height,int index)
+    protected Vector3 GetPoint(float size, float height, int index)
     {
-        float angle_deg = isFlatTopped ?  60*index : 60*index-30;
-        float angle_rad = Mathf.PI/180f * angle_deg;
-        return new Vector3((size*Mathf.Cos(angle_rad)),height,size*Mathf.Sin(angle_rad));
+        float angle_deg = isFlatTopped ? 60 * index : 60 * index - 30;
+        float angle_rad = Mathf.PI / 180f * angle_deg;
+        return new Vector3((size * Mathf.Cos(angle_rad)), height, size * Mathf.Sin(angle_rad));
     }
 
 
